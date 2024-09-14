b66f9b7d00f4fcfae1587065efc735a4ae066488
Jacob Stephens <124077831+JacobPStephens@users.noreply.github.com>
2024-09-13 20:52:19 -0400

    Update 217-ContainsDuplicate.cs

--- 217-ContainsDuplicate.cs
+++ 217-ContainsDuplicate.cs
@@ -1,3 +1,5 @@
+// Jacob Stephens - September 12, 2024
+// https://leetcode.com/problems/contains-duplicate/description/
 public class Solution {
     public bool ContainsDuplicate(int[] nums) {
         HashSet<int> numsSet = new HashSet<int>(nums);
