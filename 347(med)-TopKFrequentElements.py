f9d8933455c153e567371f3d99299eb4166c4589
JacobPStephens <jacobpstephens9@gmail.com>
2025-02-14 16:10:31 -0500

    new top k freq

--- 347(med)-TopKFrequentElements.py
+++ 347(med)-TopKFrequentElements.py
@@ -1,29 +1,25 @@
-# Jacob Stephens - September 13, 2024
-# https://leetcode.com/problems/top-k-frequent-elements/
-from collections import defaultdict
+# Jacob Stephens - February 14, 2025
+# https://leetcode.com/problems/top-k-frequent-elements/description/
+
+import heapq
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-
-        # create default dict with values of 0
-        d = defaultdict(int)
-
-        # increment frequency by 1 each time number appears in list
-        for num in nums:
-            d[num] += 1
-
-        # alist types = [(freq, num), (freq, num)]
-        alist = []
-        for num in d:
-            alist.append((d[num], num))
-
-        # sort to put most frequent elements first
-        alist.sort(reverse=True)
-
-        # print the first k indeces
-        result = []
-        for i in range(k):
-            result.append(alist[i][1])
-
-        return result
         
+        # build count dictionary
+        count = dict()
+        for num in nums:
+            if num not in count:
+                count[num] = 0
+            count[num] += 1
+
+        # create heap from counts
+        maxHeap = [(-item[1], item[0]) for item in count.items()]
+        heapq.heapify(maxHeap)
+
+        # pop k elements from heap
+        res = []
+        for _ in range(k):
+            res.append(heapq.heappop(maxHeap)[1]) 
+        return res
+
