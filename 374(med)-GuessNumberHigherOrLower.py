0ff163d266798873d257e6a676d89642d71050ec
Jacob Stephens <124077831+JacobPStephens@users.noreply.github.com>
2025-04-04 23:44:46 -0400

    binary search update

--- 374(med)-GuessNumberHigherOrLower.py
+++ 374(med)-GuessNumberHigherOrLower.py
@@ -15,13 +15,12 @@ class Solution:
         r = n
         while l <= r:
             m = (l + r) // 2
-            # uncomment the following for real solution
-            # if guess(m) == 0:
-            #     return m
-            # elif guess(m) == -1:
-            #     r = m - 1
-            # elif guess(m) == 1:
-            #     l = m + 1
+            if guess(m) == 0:
+                return m
+            elif guess(m) == -1:
+                r = m - 1
+            elif guess(m) == 1:
+                l = m + 1
             
         
 
